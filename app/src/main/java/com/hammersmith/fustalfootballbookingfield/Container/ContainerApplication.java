39855188ee52a5ffe2e3d17e609c1a3a6ce2d7cf
Chanthuon168 <chanthuonsreng@gmail.com>
2015-11-09 08:54:20 +0700

    Create ListView

--- app/src/main/java/com/hammersmith/fustalfootballbookingfield/Container/ContainerApplication.java
+++ app/src/main/java/com/hammersmith/fustalfootballbookingfield/Container/ContainerApplication.java
@@ -1,11 +1,18 @@
 package com.hammersmith.fustalfootballbookingfield.Container;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.SearchRecentSuggestions;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.widget.Toast;
 
+import com.hammersmith.fustalfootballbookingfield.app.SuggestionProvider;
 import com.hammersmith.fustalfootballbookingfield.users.MainActivity;
 import com.hammersmith.fustalfootballbookingfield.R;
 
@@ -14,10 +21,22 @@ import com.hammersmith.fustalfootballbookingfield.R;
  */
 public class ContainerApplication extends AppCompatActivity {
     private MainActivity containTab;
+    SearchView searchView;
+    SearchRecentSuggestions recentSuggestions;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.container_framelayout);
+
+        recentSuggestions = new SearchRecentSuggestions(this, SuggestionProvider.AUTHORITY,SuggestionProvider.MODE);
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) findViewById(R.id.search_view_widget);
+        searchView.setFocusable(false);
+        searchView.setQueryHint("Search");
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        SearchView.SearchAutoComplete searchAutoComplete = (SearchView.SearchAutoComplete) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+
         if(savedInstanceState == null){
             initScreen();
         }else {
@@ -43,5 +62,17 @@ public class ContainerApplication extends AppCompatActivity {
         transaction.replace(id,fragment,null);
         transaction.addToBackStack(null);
         transaction.commit();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Bundle extras = intent.getExtras();
+        String userQuery = String.valueOf(extras.get(SearchManager.USER_QUERY));
+        String query = String.valueOf(extras.get(SearchManager.QUERY));
+        recentSuggestions.saveRecentQuery(query, null);
+        Toast.makeText(this, "query: " + query + " user_query: " + userQuery,
+                Toast.LENGTH_SHORT).show();
+        searchView.setQuery(query, false); // leave query text in SearchView
     }
 }
