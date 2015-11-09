4fa02f46a65c746a700a5aab0001def8b12c1e17
Chanthuon168 <chanthuonsreng@gmail.com>
2015-11-09 13:10:30 +0700

    Create ListView

--- app/src/main/java/com/hammersmith/fustalfootballbookingfield/TabMain/TabMap.java
+++ app/src/main/java/com/hammersmith/fustalfootballbookingfield/TabMain/TabMap.java
@@ -1,19 +1,13 @@
 package com.hammersmith.fustalfootballbookingfield.TabMain;
 
-import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -25,11 +19,7 @@ import com.hammersmith.fustalfootballbookingfield.users.RootFragment;
  */
 public class TabMap extends RootFragment {
     static final LatLng TutorialsPoint = new LatLng(21 , 57);
-    private GoogleMap mMap;
-    SupportMapFragment supportMapFragment;
-    MapView mMapView;
-    Marker mMarker;
-    private static View view;
+    private GoogleMap googleMap;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,32 +32,19 @@ public class TabMap extends RootFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_map,container, false);
 
-        mMapView = (MapView) view.findViewById(R.id.maps);
-        mMapView.onCreate(savedInstanceState);
-        mMapView.onResume(); // needed to get the map to display immediately
-        MapsInitializer.initialize(getActivity());
-        mMap = mMapView.getMap();
-        mMap.setBuildingsEnabled(true);
-
-        // Latitude Logtitude
-        LatLng latLng = new LatLng(11.5565195, 104.9198001);
-       // Create Maker
-        mMarker = mMap.addMarker(new MarkerOptions().position(latLng).title("Brown Coffee"));
-        CameraPosition cameraPosition = new CameraPosition.Builder().target(latLng).zoom(15).build();
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-        GoogleMap.OnMyLocationChangeListener myLocationChangeListener = new GoogleMap.OnMyLocationChangeListener() {
-            @Override
-            public void onMyLocationChange(Location location) {
-                LatLng loc = new LatLng(location.getLatitude(),location.getLongitude());
-                mMarker = mMap.addMarker(new MarkerOptions().position(loc));
-                if (mMap != null){
-                    CameraPosition cameraPosition = new CameraPosition.Builder().target(loc).zoom(15).build();
-                    mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-                }
+        try {
+            if (googleMap == null) {
+                googleMap = ((MapFragment)getActivity().getFragmentManager().findFragmentById(R.id.maps)).getMap();
+//                googleMap = ((MapFragment) getFragmentManager().
+//                        findFragmentById(R.id.Map)).getMap();
             }
-        };
-
-
+            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            Marker TP = googleMap.addMarker(new MarkerOptions().
+                    position(TutorialsPoint).title("TutorialsPoint"));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return view;
     }
