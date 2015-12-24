708b7dff20ce3d85ae9f8b1268e7c92458327285
Chanthuon168 <chanthuonsreng@gmail.com>
2015-12-24 13:15:30 +0700

    Create adapterleague and request arrayjson

--- app/src/main/java/com/hammersmith/fustalfootballbookingfield/TabMain/TabLeague.java
+++ app/src/main/java/com/hammersmith/fustalfootballbookingfield/TabMain/TabLeague.java
@@ -13,8 +13,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.hammersmith.fustalfootballbookingfield.R;
 import com.hammersmith.fustalfootballbookingfield.adapter.AdapterLeague;
+import com.hammersmith.fustalfootballbookingfield.controller.AppController;
+import com.hammersmith.fustalfootballbookingfield.model.Field;
+import com.hammersmith.fustalfootballbookingfield.model.League;
+import com.hammersmith.fustalfootballbookingfield.utils.Constant;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by USER on 12/3/2015.
@@ -23,18 +37,53 @@ public class TabLeague extends Fragment implements AdapterLeague.ClickListener {
     RecyclerView mRecyclerView;
     LinearLayoutManager mLayoutManager;
     AdapterLeague mAdapter;
+    int[] id;
+    List<League> leagues = new ArrayList<>();
+    League league;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_league_list,container,false);
 
         mRecyclerView = (RecyclerView)view.findViewById(R.id.recylcerview);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(getActivity());
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mAdapter = new AdapterLeague();
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mAdapter = new AdapterLeague(getActivity(),leagues);
         mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setHasFixedSize(true);
         mAdapter.setClickListener(this);
+
+        if (leagues.size() <= 0) {
+            // Creating volley request obj
+            JsonArrayRequest leagueReq = new JsonArrayRequest(Constant.URL_LEAGUE, new Response.Listener<JSONArray>() {
+                @Override
+                public void onResponse(JSONArray jsonArray) {
+                    id = new int[jsonArray.length()];
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        try {
+                            JSONObject obj = jsonArray.getJSONObject(i);
+                            league = new League();
+                            league.setImage(Constant.URL_HOME + obj.getString("image_path"));
+//                            league.setTitle(obj.getString("title"));
+                            id[i] = obj.getInt("id");
+                            leagues.add(league);
+//                            Toast.makeText(getActivity(),Constant.URL_HOME+obj.getString("image_path"),Toast.LENGTH_SHORT).show();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    mAdapter.notifyDataSetChanged();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError volleyError) {
+                    Toast.makeText(getActivity(), volleyError + "", Toast.LENGTH_SHORT).show();
+                }
+            });
+            AppController.getInstance().addToRequestQueue(leagueReq);
+
+        }
 
         view.setFocusableInTouchMode(true);
         view.requestFocus();
@@ -55,40 +104,7 @@ public class TabLeague extends Fragment implements AdapterLeague.ClickListener {
 
     @Override
     public void itemClicked(View view, int position) {
-//        Toast.makeText(getActivity(), "Item  "+position, Toast.LENGTH_SHORT).show();
-        String url = "";
-        if (position == 0){
-            url = "https://www.ligabbva.com/liga-bbva/";
-        }
-        else if (position == 1){
-            url = "http://www.premierleague.com";
-        }
-        else if (position == 2){
-            url = "http://www.flashscore.com/soccer/germany/bundesliga/";
-        }
-        else if (position == 3){
-            url = "http://www.legaseriea.it/it";
-        }
-        else if (position == 4){
-            url = "http://www.uefa.com/uefachampionsleague/";
-        }
-        else if (position == 5){
-            url = "http://www.football-league.co.uk/";
-        }
-        else {
-            url = "http://www.cambodianfootball.com";
-        }
 
-        Fragment fragment = new TabLeagueView();
-        Bundle bundle = new Bundle();
-        bundle.putString("url",url);
-        fragment.setArguments(bundle);
-
-        FragmentManager fragmentManager = getChildFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.layoutLeague,fragment);
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
     }
 
 }
--- app/src/main/java/com/hammersmith/fustalfootballbookingfield/utils/Constant.java
+++ app/src/main/java/com/hammersmith/fustalfootballbookingfield/utils/Constant.java
@@ -4,9 +4,10 @@ package com.hammersmith.fustalfootballbookingfield.utils;
  * Created by Khmer on 9/16/2015.
  */
 public class Constant {
-    public static final String URL_CATEGORY = "http://192.168.1.102:81/futsal/public/category/";
-    public static final String URL_LOCATION = "http://192.168.1.102:81/futsal/public/location";
-    public static final String URL_HOME = "http://192.168.1.102:81/futsal/public/";
-    public static final String URL_FIELD_DETAIL = "http://192.168.1.102:81/futsal/public/field/";
-    public static final String URL_BOOK_DETAIL = "http://192.168.1.102:81/futsal/public/field/book/";
+    public static final String URL_CATEGORY = "http://192.168.1.113:81/futsal/public/category/";
+    public static final String URL_LOCATION = "http://192.168.1.113:81/futsal/public/location";
+    public static final String URL_HOME = "http://192.168.1.113:81/futsal/public/";
+    public static final String URL_FIELD_DETAIL = "http://192.168.1.113:81/futsal/public/field/";
+    public static final String URL_BOOK_DETAIL = "http://192.168.1.113:81/futsal/public/field/book/";
+    public static final String URL_LEAGUE = "http://192.168.1.113:81/futsal/public/league";
 }
